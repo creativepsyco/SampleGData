4820a1c9c8f2f0c0b43e4fe371cb608de56ff4f8
Mohit Singh Kanwal <mohit_kanwal@hotmail.com>
2011-02-09 18:53:23 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SampleGData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(160, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(297, 315);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create Calendar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Delete Calendar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 554);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(801, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(146, 17);
+            this.toolStripStatusLabel1.Text = "Copyright mohit kanwal 2011";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 576);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Google.GData.Client;
+using Google.GData.Calendar;
+using Google.GData.Extensions;
+using Google.AccessControl;
+
+namespace SampleGData
+{
+    public partial class Form1 : Form
+    {
+        //variables to cache the entries
+        CalendarService myService = new CalendarService("exampleCo-exampleApp-1");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void initCalendar()
+        {
+            //Logging in checks
+            myService.setUserCredentials("mohit.kanwal@gmail.com", "world1-1");
+        }
+
+        private void refreshCalendar()
+        {
+            CalendarQuery query = new CalendarQuery();
+            
+            query.Uri = new Uri("https://www.google.com/calendar/feeds/default/owncalendars/full");
+            CalendarFeed resultFeed = (CalendarFeed)myService.Query(query);
+            //Console.WriteLine("Your calendars:\n");
+            listBox1.Items.Clear();
+            foreach (CalendarEntry entry in resultFeed.Entries)
+            {
+
+                listBox1.Items.Add(entry.Title.Text);
+                richTextBox1.AppendText(entry.Content.AbsoluteUri.ToString() + "\n");
+                //richTextBox1.Text += entry.Title.Text + "\n";
+            }
+        }
+        
+        private void createNewCalendar(CalendarService service)
+        {
+            CalendarEntry calendar = new CalendarEntry();
+            calendar.Title.Text = textBox1.Text;//"Little League Schedule";
+            calendar.Summary.Text = "This calendar contains the practice schedule and game times.";
+            calendar.TimeZone = "America/Los_Angeles";
+            calendar.Hidden = false;
+            calendar.Color = "#2952A3";
+            calendar.Location = new  Where("", "", "Oakland");
+
+            Uri postUri = new Uri("https://www.google.com/calendar/feeds/default/owncalendars/full");
+            CalendarEntry createdCalendar = (CalendarEntry)service.Insert(postUri, calendar);
+            refreshCalendar();
+        }
+
+        private void deleteCalendar(CalendarService service,string calendarTitle)
+        {
+            //assume title is non-empty for now
+            CalendarQuery query = new CalendarQuery();
+            query.Uri = new Uri("https://www.google.com/calendar/feeds/default/owncalendars/full");
+            CalendarFeed resultFeed = (CalendarFeed)service.Query(query);
+
+            foreach (CalendarEntry entry in resultFeed.Entries)
+            {
+                if (entry.Title.Text == calendarTitle)
+                {
+                    try
+                    {
+                        entry.Delete();
+                    }
+                    catch (GDataRequestException)
+                    {
+                        MessageBox.Show("Unable to delete primary calendar.\n");
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            // Create a CalenderService and authenticate
+            initCalendar();
+            refreshCalendar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                createNewCalendar(myService);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            refreshCalendar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                toolStripStatusLabel1.Text = "Please Select a valid Calendar";
+            }
+            else
+            {
+                string title = listBox1.Items[listBox1.SelectedIndex].ToString();
+                deleteCalendar(myService, title);
+                refreshCalendar();
+            }
+        }
+
+        private string getUriOfSelectedCalendar(string calendarTitle)
+        {
+            CalendarQuery query = new CalendarQuery();
+
+            query.Uri = new Uri("https://www.google.com/calendar/feeds/default/owncalendars/full");
+            CalendarFeed resultFeed = (CalendarFeed)myService.Query(query);
+            foreach (CalendarEntry entry in resultFeed.Entries)
+            {
+                if (calendarTitle == entry.Title.Text)
+                    return entry.Content.AbsoluteUri;
+            }
+            return "NO";
+        }
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                toolStripStatusLabel1.Text = "No valid calendar is selected";
+            }
+            else
+            {
+                richTextBox1.Clear();
+                string calendarTitle = listBox1.Items[listBox1.SelectedIndex].ToString();
+                string uri = getUriOfSelectedCalendar(calendarTitle);
+                // Create the query object:
+                EventQuery query = new EventQuery();
+                uri.Replace("http", "https");
+                query.Uri = new Uri(uri);
+                //query.Uri = new Uri("https://www.google.com/calendar/feeds/mohit.kanwal@gmail.com/private/full");
+
+                // Tell the service to query:
+                EventFeed calFeed = myService.Query(query);
+                //add the feature for showing events here
+                foreach (EventEntry myEntry in calFeed.Entries)
+                {
+                    richTextBox1.AppendText("Title: " + myEntry.Title.Text);
+                    foreach ( When a in myEntry.Times)
+                    {
+                        richTextBox1.AppendText(a.StartTime.ToString());
+                    }
+                    
+                    richTextBox1.AppendText("\n\n");
+                    
+                }
+                
+            }
+        }
+    }
+}
